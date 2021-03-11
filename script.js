9cb23168780366a0be753ff2909521c5fa2a6ea4
Oz Ramos <midiblocks@gmail.com>
2021-03-11 10:55:45 -0800

    Control sketches on the page and in iframes

--- script.js
+++ script.js
@@ -9,14 +9,15 @@
   $link.setAttribute('type', 'text/css')
   $link.setAttribute('href', 'https://unpkg.com/handsfree@8.4.3/build/lib/assets/handsfree.css')
 
-  // Setup Handsfree
+  /**
+   * Configure Handsfree.js
+   */
   $script.onload = function () {
     handsfree = new Handsfree({
       showDebug: true,
       hands: true
     })
     handsfree.enablePlugins('browser')
-    handsfree.start()
 
     // Position fix the debugger
     handsfree.debug.$wrap.style.position = 'fixed'
@@ -24,8 +25,58 @@
     handsfree.debug.$wrap.style.right = '0'
     handsfree.debug.$wrap.style.bottom = '0'
     handsfree.debug.$wrap.style.zIndex = '99999'
+
+    /**
+     * Click and drag sketchfabs
+     */
+    const eventMap = {
+      start: 'mousedown',
+      held: 'mousemove',
+      released: 'mouseup'
+    }
+    
+    handsfree.use('sketchfab', {
+      onFrame: ({hands}) => {
+        if (!hands.multiHandLandmarks) return
+    
+        hands.pointer.forEach((pointer, hand) => {
+          if (pointer.isVisible && hands.pinchState[hand][0]) {
+            // Get the event and element to send events to
+            const event = eventMap[hands.pinchState[hand][0]]
+            const $el = document.elementFromPoint(pointer.x, pointer.y)
+            
+            // Dispatch the event
+            if ($el) {
+              let $canvas
+              
+              // Find the canvas inside the iframe
+              if ($el.tagName.toLocaleLowerCase() === 'canvas' && $el.classList.contains('canvas')) {
+                $canvas = $el
+              } else if ($el.tagName.toLocaleLowerCase() === 'iframe' && $el.src.startsWith('https://sketchfab.com/models')) {
+                $canvas = $el.contentWindow.document.querySelector('canvas.canvas')
+              }
+    
+              if ($canvas) {
+                $canvas.dispatchEvent(
+                  new MouseEvent(event, {
+                    bubbles: true,
+                    cancelable: true,
+                    clientX: pointer.x,
+                    clientY: pointer.y
+                  })
+                )  
+              }
+            }
+          }
+        })
+      }
+    })
+
+    // Start Handsfree
+    handsfree.start()
   }
 
+  // Inject Handsfree.js
   document.head.appendChild($link)
   document.body.appendChild($script)
 })()
